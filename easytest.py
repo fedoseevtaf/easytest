cea1caab5e5e8aab2dc8715c72251132ac5eba20
fedoseevtaf <76451152+fedoseevtaf@users.noreply.github.com>
2021-01-05 14:12:18 +0300

    Update easytest.py

--- easytest.py
+++ easytest.py
@@ -1,1 +1,9 @@
-
+class test():
+    def __init__(self, function, message = ''):
+        pass
+    
+    def case(self, inputData = 0, outputData = 0, sep = 0):
+        pass
+    
+    def todo(self, *skipargs, writeFile = False, openFlag = 'w', printResult = True):
+        pass
